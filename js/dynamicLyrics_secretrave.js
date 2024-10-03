46d81f68410335975dc4ee66aa5a5d8bf5c4fb86
Tofu Tony <161381360+realtofutony@users.noreply.github.com>
2024-10-02 23:55:40 -0400

    Beta 1.2.1 secretrave pt1
    added secretrave.html
    created functionality for dynamic buttons and lyrics box.
    
    ~need to fix title
    ~need to change header thing idk
    ~NEED TO DO REAL SETLIST

---
+++ js/dynamicLyrics_secretrave.js
@@ -0,0 +1,329 @@
+function displayLyrics(songKey) {
+    const lyricsData = {
+        song1: "Lyrics for Song 1...\nVerse 1...\nChorus...",
+        song2: "Lyrics for Song 2...\nVerse 1...\nChorus...",
+        song3: "Lyrics for Song 3...\nVerse 1...\nChorus...",
+        song4: "Lyrics for Song 4...\nVerse 1...\nChorus...",
+        song5: "Lyrics for Song 5...\nVerse 1...\nChorus...",
+        song6: "Lyrics for Song 6...\nVerse 1...\nChorus...",
+        song7: `Young dumb broke
+But my heater go off
+In a SRT driving round real slow
+When they see yo ass
+They gon’ pull out a pole
+Speed that ass down
+I don’t need you too loud
+When the shot go off
+It gon' be right now
+Messin with the wrong ass people
+And that shit go south
+I don’t want no hoe
+I don’t want no mouth
+Young dumb broke
+But my heater go off
+In a SRT driving round real slow
+When they see yo ass
+They gon’ pull out a pole
+Speed that ass down
+I don’t need you too loud
+When the shot go off
+It gon' be right now
+Bitch I’m counting on my LMA
+Imma add it up
+I add it up
+I add it up
+I add it up
+I add it up
+
+You were sitting back
+Acting like you jumped
+Off the fuckin’ porch
+Ayy of course
+Ayy of course
+Ayy of course
+Young dumb broke
+But my heater go off
+In a SRT driving round real slow
+When they see yo ass
+They gon’ pull out a pole
+Damn
+Put yo body in a hole
+
+First things first when I hit the stage
+I got everybody bouncing like
+They're tripped at a rave
+Keep the music loud
+'Cause I ain't come to behave
+They said Tony slow it down a sec
+Wait, okay
+
+Scandalous, keep myself in trouble
+Breaking rules like we some vandalists
+Fumbling on those beats now
+Give me the mic 'cause
+Y'all can't handle this
+Bespoke, homegrown, phenom
+Prodigy, now Imma be that
+Michigan's own Socrates
+
+Fuck with me
+Better bring it to the table if you
+Fuck with me
+Get that demo out my face
+Nah, you can't fuck with me
+Need to stop with all the talk
+Like you in love with me
+Like you in love with me
+Bitch
+Fuck with me
+Better rethink all your choices
+Don't you fuck with me
+Get that skinny fist out my face
+Don't you fuck with me
+Better stop with all that talk
+Like you could fuck with me
+‘Cause you can’t fuck with me
+Bitch
+Light 'em up, light 'em up, light 'em up
+Beat his ass down
+Make him throw up all his lunch
+If he's got some friends
+We gon' tell 'em good luck
+Because I got my army with me
+And we all suited up
+Upstage in the cut
+When I spit, bitches done for
+Then I skrrt skrt off in the 3 door
+I need more, I'm too wild, too mean
+Million other copies
+But there's just one Tony
+I'm like a
+Suicide pilot
+Dropping bombs
+On the nose dive
+Pretty soon, everyone's asking Tony for a cosign
+I'm flattered,
+But you're getting vetoed, little bitch
+Funny, think you got a chance against the executive
+Breaking limbs in the mosh pit
+DJ had to stop it
+Of course, course he had to stop it
+Of course, course he had to stop it
+Of course, of course, of course...
+
+Young dumb broke
+But my heater go off
+In a SRT driving round real slow
+When they see yo ass
+They gon’ pull out a pole
+Speed that ass down
+I don’t need you too loud
+When the shot go off
+It gon' be right now
+Bitch I’m counting on my LMA
+Imma add it up
+I add it up
+I add it up
+I add it up
+I add it up`,
+        song8: `Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+
+Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+
+Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+
+Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+
+Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+
+Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you
+
+Tell them what you want
+But we know the real truth
+Blame me all you want
+But I won’t forgive you
+When you’re all alone
+My words, do they come through
+I hope you heal soon
+I won’t forget you
+
+I won’t forget you
+I won’t forget you
+I won’t forget you
+I won’t forget you`,
+        song9: `Priceless
+Indecisive
+Worship crisis
+Follow me everywhere
+That I be hiding
+Stacking up cash
+For the first of the month
+Gotta look past
+All the diamonds and the rough
+Never on pause gotta redline
+No sleep 'til me and my dogs
+Make it big time woah (woah)
+If he's talking shit, then he's asking for a beatdown woah
+Put another hit, 'bout to cash it, but I let bro go
+Posted with his fucking throat slit, that's for sho
+Not so lucky next time, sorry, but I gotta let him know
+
+I don't come from a generation of wealth
+But a generation forced to stay in the stealth
+Barely live by themselves
+Provide for they health
+I been on a mission
+Ever since momma got cans from the Christians
+Fueled by the faces of ones we're missing
+Still have regrets, shit I should've listened
+Came from the bottom
+Had a promise never stop
+Wish I had a nest egg
+And the family drop top
+Where I go bar hop with my backwards hat
+At Harper's or Rick's where the white chicks at
+Picture that
+Juxtapose this with mismatched dads
+Don't suppose you could
+Go right back to when he met you
+Say "It's okay I'm not worth the hassle"
+
+Go ahead grab the stick, baby
+Slam the gears, stick it in, baby
+Haven't really felt my- (haven't really felt my)
+Go ahead grab the stick, baby
+Slam the gears, stick it in, baby
+Haven't really felt myself lately
+Haven't really been myself lately
+
+Priceless
+Indecisive
+Worship crisis
+Follow me everywhere
+That I be hiding
+Stacking up cash
+For the first of the month
+Gotta look past
+All the diamonds and the rough
+Never on pause gotta redline
+No sleep 'til me and my dogs
+Make it big time woah (woah)
+If he's talking shit, then he's asking for a beatdown woah
+Put another hit, 'bout to cash it, but I let bro go
+Posted with his fucking throat slit, that's for sho
+Not so lucky next time, sorry, but I gotta let him know
+
+Feint to the body and I come up top
+We can go a round, boy, I'm the cream of the crop
+Right now, right now you're an enemy
+Pretty soon I'm the one you're worshiping
+Maybe then it's all behind us
+Save for vague reminders
+Who knows down the road
+Buy some merch and sign it
+But don't get excited, no
+Baby boy left to hiding out
+Too much empathizing
+When tragedy left me blind and out
+Praying every day for some time back
+Why'd he throw it all away for a dime bag
+Decade later do the same with a coke head
+Pleading to my brothers please stay in co-ed
+Maybe that's where the pain really stems from
+Pretty tough to explain when you're that young
+Since then it's a climb up the mountain
+But it'd help if I had some
+
+Income`,
+    };
+
+    document.getElementById('lyrics-text').textContent = lyricsData[songKey] || "Lyrics not found.";
+    document.getElementById('lyrics-text').innerHTML = lyricsData[songKey].replace(/\n/g, '<br>') || "Lyrics not found.";
+
+    document.querySelectorAll('.song-button').forEach(button => {
+        button.classList.toggle('active', button.getAttribute('data-key') === songKey);
+    });
+}
+
+// Add event listeners to buttons once
+document.querySelectorAll('.song-button').forEach(button => {
+    button.addEventListener('click', () => {
+        displayLyrics(button.getAttribute('data-key'));
+    });
+});
